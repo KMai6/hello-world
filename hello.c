93ad31f4091873c0a93fd93bbdbe8f29a6a62461
KMai6 <KMai6@users.noreply.github.com>
2020-01-16 11:43:48 -0600

    Format
    Formatted it to my liking

--- hello.c
+++ hello.c
@@ -1,9 +1,12 @@
 int main(int argc, char **argv) {
-  printf("Kevin Mai");
-  printf("Software Engineering");
-  printf("Alex Stumpff");
-  printf("Undeclared");
-  printf("Maxx Seminario\n", );
-  printf("Electrical Engineering\n", );
+  printf("Kevin Mai\n");
+  printf("Software Engineering\n");
+
+  printf("Alex Stumpff\n");
+  printf("Undeclared\n");
+
+  printf("Maxx Seminario\n");
+  printf("Electrical Engineering\n");
+  
   return 0;
 }
